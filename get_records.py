58ae29213580d12ef08628023de947a87f759186
marinasemak <marina.semak@gmail.com>
2024-10-23 18:17:21 +0300

    add redis cache

--- get_records.py
+++ get_records.py
@@ -1,6 +1,27 @@
 from seed import Author, Quote
+import redis
+from redis_lru import RedisLRU
 
+# Redis server configuration
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
+CACHE_MAXSIZE = 100  # Maximum number of items to store in the cache
 
+# Create a Redis client
+try:
+    redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
+    # Test the connection
+    redis_client.ping()
+    print("Connected to Redis successfully.")
+except redis.exceptions.ConnectionError as e:
+    print(f"Failed to connect to Redis: {e}")
+    exit(1)
+
+# Initialize RedisLRU with the Redis client and maximum cache size
+cache = RedisLRU(redis_client)
+
+@cache
 def get_all_author_quotes(name):
     author = Author.objects(fullname=name).first()
     if not author:
@@ -11,14 +32,14 @@ def get_all_author_quotes(name):
     for quote in quotes:
         print(quote.quote)
 
-
+@cache
 def get_quotes_by_tag(tag):
     quotes = Quote.objects(tags=tag)
     print(f"Quotes for {tag}:")
     for quote in quotes:
         print(quote.quote)
 
-
+@cache
 def get_quotes_by_all_tags(tags):
     quotes = Quote.objects(tags__in=tags)
     print(f"Quotes for {tags}:")
