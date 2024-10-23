d3837dcd6d10ece11447d00f7a73635ff60905ef
marinasemak <marina.semak@gmail.com>
2024-10-23 22:06:09 +0300

    fix redis caching

--- get_records.py
+++ get_records.py
@@ -23,25 +23,23 @@ cache = RedisLRU(redis_client)
 
 @cache
 def get_all_author_quotes(name):
+    print('dfsdf')
     author = Author.objects(fullname=name).first()
     if not author:
         print(f'No author found with such name "{name}"')
         return
     quotes = Quote.objects(author=author)
-    print(f"Quotes of {name}:")
-    for quote in quotes:
-        print(quote.quote)
+    result = [quote.quote for quote in quotes]
+    return result
 
 @cache
 def get_quotes_by_tag(tag):
     quotes = Quote.objects(tags=tag)
-    print(f"Quotes for {tag}:")
-    for quote in quotes:
-        print(quote.quote)
+    result = [quote.quote for quote in quotes]
+    return result
 
 @cache
 def get_quotes_by_all_tags(tags):
     quotes = Quote.objects(tags__in=tags)
-    print(f"Quotes for {tags}:")
-    for quote in quotes:
-        print(quote.quote)
+    result = [quote.quote for quote in quotes]
+    return result
--- main.py
+++ main.py
@@ -11,11 +11,20 @@ def main():
             case "exit":
                 break
             case "name":
-                get_all_author_quotes(value[0].lstrip())
+                name_value = value[0].lstrip()
+                result = get_all_author_quotes(name_value)
+                print(f"Quotes for name '{name_value}':")
+                print(result)
             case "tag":
-                get_quotes_by_tag(value[0].lstrip())
+                tag_value = value[0].lstrip()
+                result = get_quotes_by_tag(tag_value)
+                print(f"Quotes for tag '{tag_value}':")
+                print(result)
             case "tags":
-                get_quotes_by_all_tags(value[0].lstrip().split(","))
+                tags_value = value[0].lstrip().split(",")
+                result = get_quotes_by_all_tags(tags_value)
+                print(f"Quotes for tags {tags_value}:")
+                print(result)
             case _:
                 print("Invalid command")
 
